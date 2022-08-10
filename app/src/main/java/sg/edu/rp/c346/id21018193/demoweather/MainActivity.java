b407d4d72d87d251e097c21e6889222ef657a5be
lee zhuo hong <21018193@myrp.edu.sg>
2022-08-10 09:40:44 +0800

    commit by zh

--- app/src/main/java/sg/edu/rp/c346/id21018193/demoweather/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/demoweather/MainActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import com.loopj.android.http.AsyncHttpClient;
+
 public class MainActivity extends AppCompatActivity {
 
     ListView lvWeather;
+    AsyncHttpClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +18,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         lvWeather = findViewById(R.id.listviewWeather);
+        client = new AsyncHttpClient();
     }
 }
